3cf9fbd89e20062d2e77291f6d9fc8ea35551003
wikky2012 <wicliferono@gmail.com>
2020-07-24 11:56:07 +0300

    add what we do content in the index page and the JavaScript responsible for it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
-        $("img").click(function() {
-          $(".design-showing").toggle();
-          $(".design-hidden").toggle();
+        $("img#image1").click(function() {
+          $(".design-showing1").toggle();
+          $(".design-hidden1").toggle();
         });
-        $("p").click(function() {
-            $(".design-showing").toggle();
-            $(".design-hidden").toggle();
-          });
+        $("p#one").click(function() {
+          $(".design-showing1").toggle();
+          $(".design-hidden1").toggle();
+        });
+        $("img#image2").click(function() {
+          $(".design-showing2").toggle();
+          $(".design-hidden2").toggle();
+        });
+        $("p#two").click(function() {
+          $(".design-showing2").toggle();
+          $(".design-hidden2").toggle();
+        });
+        $("img#image3").click(function() {
+          $(".design-showing3").toggle();
+          $(".design-hidden3").toggle();
+        });
+        $("p#three").click(function() {
+            $(".design-showing3").toggle();
+            $(".design-hidden3").toggle();
+        });
       });
 
